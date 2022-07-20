30c38f33a9bfdc22f98dbf16493aef32d9ca3f30
Inu-90 <ismailinu49@gmail.com>
2022-07-20 13:13:28 +0100

    Updated modification of the interface

--- converter.js
+++ converter.js
@@ -8,17 +8,17 @@ let fahrenheitSelect = document.querySelector('.radio_fahrenheit');
 let fahrenheitInput = document.querySelector('.fahrenheit_label');
 let fahrenheitInput2 = document.getElementById('fahrenheit_label2');
 
-// SUBMIT AND CLEAR SELECTORS
-let submit = document.querySelector('.convert_button');
-let clear = document.querySelector('.clear');
+// convert AND reset SELECTORS
+let convert = document.querySelector('.convert');
+let reset = document.querySelector('.reset');
 
 // SWIPE SELECTORS
 let flip1 = document.querySelector('.flip1');
 let flip2 = document.querySelector('.flip2');
 
-// CELSIUS CONTROLLER
+//CELSIUS CONTROLLER
   celsiusSelect.addEventListener('click', () => {
-  flip1.style.display = 'block';
+  flip1.style.display = 'grid';
   flip2.style.display = 'none';
   celsiusInput.disabled = false;
   });
@@ -26,32 +26,32 @@ let flip2 = document.querySelector('.flip2');
 // FAHRENHEIT CONTROLLER
 fahrenheitSelect.addEventListener('click', () => {
   flip1.style.display = 'none';
-  flip2.style.display = 'block';
+  flip2.style.display = 'grid';
   celsiusInput2.disabled = false;
   fahrenheitInput2.disabled = true;
 })
 
-submit.addEventListener('click', () => { 
-  if(flip1.style.display === 'block' && flip2.style.display === 'none') {
+convert.addEventListener('click', () => { 
+  if(flip1.style.display === 'grid' && flip2.style.display === 'none') {
     const fahr = ((1.8 * (celsiusInput.value)) + 32);
   fahrenheitInput.value = (fahr.toFixed(2));
-  } else if(flip2.style.display === 'block' && flip1.style.display === 'none') {
+  } else if(flip2.style.display === 'grid' && flip1.style.display === 'none') {
     const cels = ((celsiusInput2.value - 32) / 1.8);
     fahrenheitInput2.value = (cels.toFixed(2));
   }
   });
 
   document.addEventListener('keypress', event => {
-    if (event.keyCode === 13 && flip1.style.display === 'block' && flip2.style.display === 'none') {
+    if (event.keyCode === 13 && flip1.style.display === 'grid' && flip2.style.display === 'none') {
       const fahr = ((1.8 * (celsiusInput.value)) + 32);
        fahrenheitInput.value = (fahr.toFixed(2));
-    } else if (event.keyCode === 13 && flip2.style.display === 'block' && flip1.style.display === 'none') {
+    } else if (event.keyCode === 13 && flip2.style.display === 'grid' && flip1.style.display === 'none') {
       const cels = ((celsiusInput2.value - 32) / 1.8);
     fahrenheitInput2.value = (cels.toFixed(2));
     }
 });
 
-clear.addEventListener('click', event => {
+reset.addEventListener('click', event => {
     celsiusInput.value = '0.0';
      fahrenheitInput.value = '0.0';
      celsiusInput2.value = '0.0';
